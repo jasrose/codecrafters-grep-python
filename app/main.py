9a159f24514dedb091befab49b7b9867aa9d0807
Jasmine Rose <jasmine.raquel.rose@gmail.com>
2024-08-22 21:42:43 -0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,6 +6,8 @@ def match_pattern(input_line, pattern):
         return pattern in input_line
     elif pattern  == "\\d":
         return any(c.isdigit() for c in input_line)
+    elif pattern == "\\w":
+        return any(c.isalpha() for c in input_line)
     else:
         raise RuntimeError(f"Unhandled pattern: {pattern}")
 
